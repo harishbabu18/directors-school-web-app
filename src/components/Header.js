91850df78864d6381bf89cbaedd01c91697052c3
B K HARISH BABU <babuamuda@gmail.com>
2024-12-26 02:21:18 +0530

    Second commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,45 +1,31 @@
-import React from "react"
+import React from "react";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 export default function Header() {
   return (
     <>
     <header id="header" className="header d-flex align-items-center sticky-top">
-  <div className="container-fluid container-xl position-relative d-flex align-items-center">
-    <a href="/" className="logo d-flex align-items-center me-auto">
-      {/* Uncomment the line below if you also wish to use an image logo */}
-      {/* <img src="assets/img/logo.png" alt=""> */}
-      <h1 className="sitename">Director's School</h1>
-    </a>
-    <nav id="navmenu" className="navmenu">
-      <ul>
-        <li>
-          <a href="/" className="active">
-            Home
-            <br />
-          </a>
-        </li>
-        <li>
-          <a href="/about">About</a>
-        </li>
-        <li>
-          <a href="/courses">Courses</a>
-        </li>
-        <li>
-          <a href="/trainers">Trainers</a>
-        </li>
-        <li>
-          <a href="/events">Events</a>
-        </li>
-        <li>
-          <a href="/contact">Contact</a>
-        </li>
-      </ul>
-      <i className="mobile-nav-toggle d-xl-none bi bi-list" />
-    </nav>
-    <a className="btn-getstarted" href="">
-      Get Started
-    </a>
-  </div>
+   
+    <Navbar id="navmenu" className="navmenu">
+      <Container>
+    <Navbar.Brand href="/" className="logo d-flex align-items-center me-auto"><h1 className="sitename">Director's School</h1></Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
+            <Nav.Link href="/courses">Couses</Nav.Link>
+            <Nav.Link href="/trainers">Trainers</Nav.Link>
+            <Nav.Link href="/events">Events</Nav.Link>
+            <Nav.Link href="/contact">Contact</Nav.Link>
+          </Nav>
+
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
 </header>
     </>
   )
---
+++ src/pages/trainers.js
@@ -0,0 +1,16 @@
+import * as React from "react"
+import WebsiteLayout from "../components/WebsiteLayout"
+
+const TrainersPage = () => {
+  return (
+    <WebsiteLayout>
+    <main>
+   <h1>TrainersPage</h1>
+    </main>
+    </WebsiteLayout>
+  )
+}
+
+export default TrainersPage
+
+export const Head = () => <title>TrainersPage</title>
